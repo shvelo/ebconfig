1300b1c1acd0174e5357ef23876b3b9cb5b471a6
Alex Art <elennaro@mail.ru>
2015-12-13 00:22:42 +0400

    * npm symlink problem fix I hope

--- 50npm.sh
+++ 50npm.sh
@@ -3,18 +3,18 @@
 function error_exit
 {
   echo $1
-  eventHelper.py --msg "$1" --severity ERROR
+  eventHelper.py --msg "$1" --severity WARN
   exit $2
 }
 
 #install not-installed yet app node_modules
-echo "install not-installed yet app node_modules"
-if [ ! -d "/var/node_modules" ]; then
-  mkdir /var/node_modules ;
-fi
-if [ -d /tmp/deployment/application ]; then
-  ln -s /var/node_modules /tmp/deployment/application/
-fi
+#echo "install not-installed yet app node_modules"
+#if [ ! -d "/var/node_modules" ]; then
+#  mkdir /var/node_modules ;
+#fi
+#if [ -d /tmp/deployment/application ]; then
+#  ln -s /var/node_modules /tmp/deployment/application/
+#fi
 
 echo "run npm install"
 OUT=$([ -d "/tmp/deployment/application" ] && cd /tmp/deployment/application && /opt/elasticbeanstalk/node-install/node-v$NODE_VER-linux-$ARCH/bin/npm install 2>&1) || error_exit "Failed to run npm install.  $OUT" $?
